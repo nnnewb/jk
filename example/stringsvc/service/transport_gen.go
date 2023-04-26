cddc505b1f6f0318c6c88853e700e1f23ab73202
weak_ptr <weak_ptr@outlook.com>
2023-04-26 18:16:04 +0800

    refactor: Use generics to reduce the amount of code generated

--- example/stringsvc/service/transport_gen.go
+++ example/stringsvc/service/transport_gen.go
@@ -12,15 +12,31 @@ import (
 	"net/url"
 )
 
-func MakeBuyRemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
+func makeHandlerFunc[REQ any, RESP any](f func(context.Context, REQ) (RESP, error)) http.HandlerFunc {
+	return func(wr http.ResponseWriter, request *http.Request) {
+		var payload REQ
+		err := json.NewDecoder(request.Body).Decode(&payload)
+		if err != nil {
+			panic(errors.Errorf("unexpected unmarshal error %+v", err))
+		}
+
+		resp, err := f(request.Context(), payload)
+		err = json.NewEncoder(wr).Encode(resp)
+		if err != nil {
+			panic(errors.Errorf("unexpected unmarshal error %+v", err))
+		}
+	}
+}
+
+func makeRemoteEndpoint[REQ any, RESP any](remoteUrl string, client *http.Client) endpoint.Endpoint {
 	return func(ctx context.Context, req interface{}) (interface{}, error) {
 		buffer := bytes.NewBufferString("")
-		u := url.URL{
-			Host:   host,
-			Path:   "/api/v1/service/buy",
-			Scheme: "https",
+		err := json.NewEncoder(buffer).Encode(req)
+		if err != nil {
+			panic(errors.Errorf("unexpected marshal error %+v", err))
 		}
-		request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
+
+		request, err := http.NewRequestWithContext(ctx, http.MethodPost, remoteUrl, buffer)
 		if err != nil {
 			return nil, errors.Trace(err)
 		}
@@ -35,293 +51,63 @@ func MakeBuyRemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
 			return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
 		}
 
-		var resp BuyResponse
+		var resp RESP
 		err = json.NewDecoder(response.Body).Decode(&resp)
 		if err != nil {
 			return nil, errors.Trace(err)
 		}
 
 		return resp, nil
-	}
-}
-
-func MakeBuyHandlerFunc(svc Service) http.HandlerFunc {
-	return func(wr http.ResponseWriter, req *http.Request) {
-		defer req.Body.Close()
-		var payload BuyRequest
-		err := json.NewDecoder(req.Body).Decode(&payload)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-		resp, err := svc.Buy(req.Context(), payload)
-		err = json.NewEncoder(wr).Encode(resp)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-	}
-}
-
-func MakeJoinRemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
-	return func(ctx context.Context, req interface{}) (interface{}, error) {
-		buffer := bytes.NewBufferString("")
-		u := url.URL{
-			Host:   host,
-			Path:   "/api/v1/service/join",
-			Scheme: "https",
-		}
-		request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		response, err := client.Do(request)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		defer response.Body.Close()
-		if response.StatusCode != http.StatusOK {
-			return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
-		}
-
-		var resp JoinResponse
-		err = json.NewDecoder(response.Body).Decode(&resp)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		return resp, nil
-	}
-}
-
-func MakeJoinHandlerFunc(svc Service) http.HandlerFunc {
-	return func(wr http.ResponseWriter, req *http.Request) {
-		defer req.Body.Close()
-		var payload JoinRequest
-		err := json.NewDecoder(req.Body).Decode(&payload)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-		resp, err := svc.Join(req.Context(), payload)
-		err = json.NewEncoder(wr).Encode(resp)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-	}
-}
-
-func MakeJoin2RemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
-	return func(ctx context.Context, req interface{}) (interface{}, error) {
-		buffer := bytes.NewBufferString("")
-		u := url.URL{
-			Host:   host,
-			Path:   "/api/v1/service/join-2",
-			Scheme: "https",
-		}
-		request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		response, err := client.Do(request)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		defer response.Body.Close()
-		if response.StatusCode != http.StatusOK {
-			return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
-		}
-
-		var resp Join2Response
-		err = json.NewDecoder(response.Body).Decode(&resp)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		return resp, nil
-	}
-}
-
-func MakeJoin2HandlerFunc(svc Service) http.HandlerFunc {
-	return func(wr http.ResponseWriter, req *http.Request) {
-		defer req.Body.Close()
-		var payload Join2Request
-		err := json.NewDecoder(req.Body).Decode(&payload)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-		resp, err := svc.Join2(req.Context(), payload)
-		err = json.NewEncoder(wr).Encode(resp)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-	}
-}
-
-func MakeJoin3RemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
-	return func(ctx context.Context, req interface{}) (interface{}, error) {
-		buffer := bytes.NewBufferString("")
-		u := url.URL{
-			Host:   host,
-			Path:   "/api/v1/service/join-3",
-			Scheme: "https",
-		}
-		request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		response, err := client.Do(request)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		defer response.Body.Close()
-		if response.StatusCode != http.StatusOK {
-			return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
-		}
-
-		var resp Join3Response
-		err = json.NewDecoder(response.Body).Decode(&resp)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		return resp, nil
-	}
-}
-
-func MakeJoin3HandlerFunc(svc Service) http.HandlerFunc {
-	return func(wr http.ResponseWriter, req *http.Request) {
-		defer req.Body.Close()
-		var payload Join3Request
-		err := json.NewDecoder(req.Body).Decode(&payload)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-		resp, err := svc.Join3(req.Context(), payload)
-		err = json.NewEncoder(wr).Encode(resp)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-	}
-}
-
-func MakeLowercaseRemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
-	return func(ctx context.Context, req interface{}) (interface{}, error) {
-		buffer := bytes.NewBufferString("")
-		u := url.URL{
-			Host:   host,
-			Path:   "/api/v1/service/lowercase",
-			Scheme: "https",
-		}
-		request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		response, err := client.Do(request)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		defer response.Body.Close()
-		if response.StatusCode != http.StatusOK {
-			return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
-		}
-
-		var resp LowercaseResponse
-		err = json.NewDecoder(response.Body).Decode(&resp)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		return resp, nil
-	}
-}
-
-func MakeLowercaseHandlerFunc(svc Service) http.HandlerFunc {
-	return func(wr http.ResponseWriter, req *http.Request) {
-		defer req.Body.Close()
-		var payload LowercaseRequest
-		err := json.NewDecoder(req.Body).Decode(&payload)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-		resp, err := svc.Lowercase(req.Context(), payload)
-		err = json.NewEncoder(wr).Encode(resp)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-	}
-}
-
-func MakeUppercaseRemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
-	return func(ctx context.Context, req interface{}) (interface{}, error) {
-		buffer := bytes.NewBufferString("")
-		u := url.URL{
-			Host:   host,
-			Path:   "/api/v1/service/uppercase",
-			Scheme: "https",
-		}
-		request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		response, err := client.Do(request)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		defer response.Body.Close()
-		if response.StatusCode != http.StatusOK {
-			return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
-		}
-
-		var resp UppercaseResponse
-		err = json.NewDecoder(response.Body).Decode(&resp)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-
-		return resp, nil
-	}
-}
-
-func MakeUppercaseHandlerFunc(svc Service) http.HandlerFunc {
-	return func(wr http.ResponseWriter, req *http.Request) {
-		defer req.Body.Close()
-		var payload UppercaseRequest
-		err := json.NewDecoder(req.Body).Decode(&payload)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
-		resp, err := svc.Uppercase(req.Context(), payload)
-		err = json.NewEncoder(wr).Encode(resp)
-		if err != nil {
-			panic(errors.Errorf("unexpected unmarshal error %+v", err))
-		}
 	}
 }
 
 func NewClient(host string, client *http.Client) Service {
+	urlBuy := url.URL{
+		Host:   host,
+		Path:   "/api/v1/service/buy",
+		Scheme: "https",
+	}
+	urlJoin := url.URL{
+		Host:   host,
+		Path:   "/api/v1/service/join",
+		Scheme: "https",
+	}
+	urlJoin2 := url.URL{
+		Host:   host,
+		Path:   "/api/v1/service/join-2",
+		Scheme: "https",
+	}
+	urlJoin3 := url.URL{
+		Host:   host,
+		Path:   "/api/v1/service/join-3",
+		Scheme: "https",
+	}
+	urlLowercase := url.URL{
+		Host:   host,
+		Path:   "/api/v1/service/lowercase",
+		Scheme: "https",
+	}
+	urlUppercase := url.URL{
+		Host:   host,
+		Path:   "/api/v1/service/uppercase",
+		Scheme: "https",
+	}
 	return EndpointSet{
-		BuyEndpoint:       MakeBuyRemoteEndpoint(host, client),
-		Join2Endpoint:     MakeJoin2RemoteEndpoint(host, client),
-		Join3Endpoint:     MakeJoin3RemoteEndpoint(host, client),
-		JoinEndpoint:      MakeJoinRemoteEndpoint(host, client),
-		LowercaseEndpoint: MakeLowercaseRemoteEndpoint(host, client),
-		UppercaseEndpoint: MakeUppercaseRemoteEndpoint(host, client),
+		BuyEndpoint:       makeRemoteEndpoint[BuyRequest, BuyResponse](urlBuy.String(), client),
+		Join2Endpoint:     makeRemoteEndpoint[Join2Request, Join2Response](urlJoin2.String(), client),
+		Join3Endpoint:     makeRemoteEndpoint[Join3Request, Join3Response](urlJoin3.String(), client),
+		JoinEndpoint:      makeRemoteEndpoint[JoinRequest, JoinResponse](urlJoin.String(), client),
+		LowercaseEndpoint: makeRemoteEndpoint[LowercaseRequest, LowercaseResponse](urlLowercase.String(), client),
+		UppercaseEndpoint: makeRemoteEndpoint[UppercaseRequest, UppercaseResponse](urlUppercase.String(), client),
 	}
 }
+
 func Register(svc Service, m *http.ServeMux) *http.ServeMux {
-	m.HandleFunc("/api/v1/service/buy", MakeBuyHandlerFunc(svc))
-	m.HandleFunc("/api/v1/service/join", MakeJoinHandlerFunc(svc))
-	m.HandleFunc("/api/v1/service/join-2", MakeJoin2HandlerFunc(svc))
-	m.HandleFunc("/api/v1/service/join-3", MakeJoin3HandlerFunc(svc))
-	m.HandleFunc("/api/v1/service/lowercase", MakeLowercaseHandlerFunc(svc))
-	m.HandleFunc("/api/v1/service/uppercase", MakeUppercaseHandlerFunc(svc))
+	m.HandleFunc("/api/v1/service/buy", makeHandlerFunc(svc.Buy))
+	m.HandleFunc("/api/v1/service/join", makeHandlerFunc(svc.Join))
+	m.HandleFunc("/api/v1/service/join-2", makeHandlerFunc(svc.Join2))
+	m.HandleFunc("/api/v1/service/join-3", makeHandlerFunc(svc.Join3))
+	m.HandleFunc("/api/v1/service/lowercase", makeHandlerFunc(svc.Lowercase))
+	m.HandleFunc("/api/v1/service/uppercase", makeHandlerFunc(svc.Uppercase))
 	return m
 }
--- internal/gen/transport.go
+++ internal/gen/transport.go
@@ -9,156 +9,164 @@ import (
 
 func GenerateTransportLayerHTTP(f *jen.File, svc *types.Named) error {
 	var (
-		iface             = svc.Underlying().(*types.Interface)
-		svcName           = svc.Obj().Name()
-		pkgPath           = svc.Obj().Pkg().Path()
-		jenHandlerFunc    = jen.Qual("net/http", "HandlerFunc")     // type http.HandlerFunc
-		jenResponseWriter = jen.Qual("net/http", "ResponseWriter")  // type http.ResponseWriter
-		jenRequestPtr     = jen.Op("*").Qual("net/http", "Request") // type *http.Request
+		iface   = svc.Underlying().(*types.Interface)
+		svcName = svc.Obj().Name()
+		pkgPath = svc.Obj().Pkg().Path()
 	)
 
-	for i := 0; i < iface.NumMethods(); i++ {
-		method := iface.Method(i)
-		signature := method.Type().Underlying().(*types.Signature)
-		params := signature.Params()
-		results := signature.Results()
+	// makeHandlerFunc[REQ,RESP any](f func(context.Context, REQ) (RESP, error)) http.HandlerFunc {
+	f.Func().Id("makeHandlerFunc").
+		Types(jen.Id("REQ").Any(), jen.Id("RESP").Any()).
+		Params(jen.Id("f").
+			Func().
+			Params(jen.Qual("context", "Context"), jen.Id("REQ")).
+			Params(jen.Id("RESP"), jen.Error())).
+		Params(jen.Qual("net/http", "HandlerFunc")).
+		BlockFunc(func(g *jen.Group) {
+			// return func(wr http.ResponseWriter, request *http.Request) {
+			g.Return(
+				jen.Func().
+					Params(
+						jen.Id("wr").Qual("net/http", "ResponseWriter"),
+						jen.Id("request").Op("*").Qual("net/http", "Request"),
+					)).
+				BlockFunc(func(g *jen.Group) {
+					// var payload REQ
+					g.Var().Id("payload").Id("REQ")
+					// err := json.NewDecoder(req.Body).Decode(&payload)
+					g.Id("err").Op(":=").
+						Qual("encoding/json", "NewDecoder").Call(jen.Id("request").Dot("Body")).
+						Dot("Decode").Call(jen.Op("&").Id("payload"))
+					// if err != nil {
+					g.If(jen.Id("err").Op("!=").Nil()).BlockFunc(func(g *jen.Group) {
+						g.Panic(jen.Qual("github.com/juju/errors", "Errorf").Call(
+							jen.Lit("unexpected unmarshal error %+v"), jen.Id("err")))
+					}).Line()
+					// resp, err := svc.Method(request.Context(), payload)
+					g.List(jen.Id("resp"), jen.Id("err")).Op(":=").
+						Id("f").Call(
+						jen.Id("request").Dot("Context").Call(),
+						jen.Id("payload"))
+					// err = json.NewEncoder(wr).Encode(resp)
+					g.Id("err").Op("=").
+						Qual("encoding/json", "NewEncoder").Call(jen.Id("wr")).
+						Dot("Encode").Call(jen.Id("resp"))
+					// if err != nil {
+					g.If(jen.Id("err").Op("!=").Nil()).BlockFunc(func(g *jen.Group) {
+						g.Panic(jen.Qual("github.com/juju/errors", "Errorf").Call(
+							jen.Lit("unexpected unmarshal error %+v"), jen.Id("err")))
+					})
+				})
+		}).Line()
 
-		// func MakeMethodRemoteEndpoint(host string, client *http.Client) endpoint.Endpoint {
-		f.Func().Id(fmt.Sprintf("Make%sRemoteEndpoint", method.Name())).
-			Params(jen.Id("host").String(), jen.Id("client").Op("*").Qual("net/http", "Client")).
-			Qual("github.com/go-kit/kit/endpoint", "Endpoint").
-			BlockFunc(func(g *jen.Group) {
-				// return func(ctx context.Context, req interface{}) (interface{}, error) {
-				g.Return(jen.Func().
-					Params(jen.Id("ctx").Qual("context", "Context"), jen.Id("req").Interface()).
-					Params(jen.Interface(), jen.Error()).
-					BlockFunc(func(g *jen.Group) {
-						// buffer := bytes.NewBufferString("")
-						g.Id("buffer").Op(":=").Qual("bytes", "NewBufferString").Call(jen.Lit(""))
-						// u := url.URL {
-						g.Id("u").Op(":=").Qual("net/url", "URL").Values(
-							jen.Dict{
-								jen.Id("Scheme"): jen.Lit("https"),
-								jen.Id("Host"):   jen.Id("host"),
-								jen.Id("Path"): jen.Lit(
-									fmt.Sprintf(
-										"/api/v1/%s/%s",
-										strcase.ToKebab(svcName),
-										strcase.ToKebab(method.Name()),
-									)),
-							},
+	f.Func().Id("makeRemoteEndpoint").
+		Types(jen.Id("REQ").Any(), jen.Id("RESP").Any()).
+		Params(jen.Id("remoteUrl").String(), jen.Id("client").Op("*").Qual("net/http", "Client")).
+		Qual("github.com/go-kit/kit/endpoint", "Endpoint").
+		BlockFunc(func(g *jen.Group) {
+			// return func(ctx context.Context, req interface{}) (interface{}, error) {
+			g.Return(jen.Func().
+				Params(jen.Id("ctx").Qual("context", "Context"), jen.Id("req").Interface()).
+				Params(jen.Interface(), jen.Error()).
+				BlockFunc(func(g *jen.Group) {
+					// buffer := bytes.NewBufferString("")
+					g.Id("buffer").Op(":=").Qual("bytes", "NewBufferString").Call(jen.Lit(""))
+					// err := json.NewEncoder(buffer).Encode(req)
+					g.Id("err").Op(":=").
+						Qual("encoding/json", "NewEncoder").Call(jen.Id("buffer")).
+						Dot("Encode").Call(jen.Id("req"))
+					// if err != nil {
+					g.If(jen.Id("err").Op("!=").Nil()).BlockFunc(func(g *jen.Group) {
+						g.Panic(jen.Qual("github.com/juju/errors", "Errorf").Call(
+							jen.Lit("unexpected marshal error %+v"), jen.Id("err")))
+					}).Line()
+					// request := http.NewRequestWithContext(ctx, http.MethodPost, remoteUrl, buffer)
+					g.List(jen.Id("request"), jen.Id("err")).Op(":=").Qual("net/http", "NewRequestWithContext").
+						Call(
+							jen.Id("ctx"),
+							jen.Qual("net/http", "MethodPost"),
+							jen.Id("remoteUrl"),
+							jen.Id("buffer"),
 						)
-						// request := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-						g.List(jen.Id("request"), jen.Id("err")).Op(":=").Qual("net/http", "NewRequestWithContext").
-							Call(
-								jen.Id("ctx"),
-								jen.Qual("net/http", "MethodPost"),
-								jen.Id("u").Dot("String").Call(),
-								jen.Id("buffer"),
-							)
-						// if err != nil {
-						g.If(jen.Id("err").Op("!=").Nil()).
-							BlockFunc(func(g *jen.Group) {
-								// return nil, errors.Trace(err)
-								g.Return(jen.Nil(), jen.Qual("github.com/juju/errors", "Trace").Call(jen.Id("err")))
-							}).Line()
-						// response, err := client.Do(request)
-						g.List(jen.Id("response"), jen.Id("err")).Op(":=").
-							Id("client").Dot("Do").Call(jen.Id("request"))
-						// if err != nil {
-						g.If(jen.Id("err").Op("!=").Nil()).
-							BlockFunc(func(g *jen.Group) {
-								// return nil, errors.Trace(err)
-								g.Return(jen.Nil(), jen.Qual("github.com/juju/errors", "Trace").Call(jen.Id("err")))
-							}).Line()
-						// defer response.Body.Close()
-						g.Defer().Id("response").Dot("Body").Dot("Close").Call()
-						// if response.StatusCode != http.StatusOK {
-						g.If(jen.Id("response").Dot("StatusCode")).Op("!=").Qual("net/http", "StatusOK").
-							BlockFunc(func(g *jen.Group) {
-								// return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
-								g.Return(
-									jen.Nil(),
-									jen.Qual("github.com/juju/errors", "Errorf").Call(
-										jen.Lit("call remote endpoint failed, http status %d %s"),
-										jen.Id("response").Dot("StatusCode"),
-										jen.Id("response").Dot("Status"),
-									),
-								)
-							}).Line()
-						// var resp MethodResponse
-						g.Var().Id("resp").Add(generateTypeCode(results.At(0).Type()))
-						// err = json.NewDecoder(response.Body).Decode(&resp)
-						g.Id("err").Op("=").Qual("encoding/json", "NewDecoder").
-							Call(jen.Id("response").Dot("Body")).
-							Dot("Decode").Call(jen.Op("&").Id("resp"))
-						// if err != nil {
-						g.If(jen.Id("err").Op("!=").Nil()).
-							BlockFunc(func(g *jen.Group) {
-								// return nil, errors.Trace(err)
-								g.Return(jen.Nil(), jen.Qual("github.com/juju/errors", "Trace").Call(jen.Id("err")))
-							}).Line()
-						// return resp, nil
-						g.Return(jen.Id("resp"), jen.Nil())
-					}))
-			}).Line()
-
-		// func MakeMethodHandler(svc pkg.Service) http.HandlerFunc {
-		f.Func().Id(fmt.Sprintf("Make%sHandlerFunc", method.Name())).
-			Params(jen.Id("svc").Qual(pkgPath, svcName)).
-			Params(jenHandlerFunc).
-			BlockFunc(func(g *jen.Group) {
-				// return func(wr http.ResponseWriter, req *http.Request) {
-				g.Return(
-					jen.Func().
-						Params(jen.Id("wr").Add(jenResponseWriter), jen.Id("req").Add(jenRequestPtr)).
+					// if err != nil {
+					g.If(jen.Id("err").Op("!=").Nil()).
 						BlockFunc(func(g *jen.Group) {
-							// defer req.Body.Close()
-							g.Defer().Id("req").Dot("Body").Dot("Close").Call()
-							// var payload RequestType
-							g.Var().Id("payload").Add(generateTypeCode(params.At(1).Type()))
-							// err := json.NewDecoder(req.Body).Decode(&payload)
-							g.Id("err").Op(":=").
-								Qual("encoding/json", "NewDecoder").Call(jen.Id("req").Dot("Body")).
-								Dot("Decode").Call(jen.Op("&").Id("payload"))
-							// if err != nil {
-							//     panic(fmt.Errorf("unexpected unmarshal error %+v", err))
-							// }
-							g.If(jen.Id("err").Op("!=").Nil()).BlockFunc(func(g *jen.Group) {
-								g.Panic(jen.Qual("github.com/juju/errors", "Errorf").Call(
-									jen.Lit("unexpected unmarshal error %+v"), jen.Id("err")))
-							})
-							// resp, err := svc.Method(req.Context(), payload)
-							g.List(jen.Id("resp"), jen.Id("err")).Op(":=").
-								Id("svc").Dot(method.Name()).Call(
-								jen.Id("req").Dot("Context").Call(),
-								jen.Id("payload"))
-							// err = json.NewEncoder(wr).Encode(resp)
-							g.Id("err").Op("=").
-								Qual("encoding/json", "NewEncoder").Call(jen.Id("wr")).
-								Dot("Encode").Call(jen.Id("resp"))
-							// if err != nil {
-							//     panic(fmt.Errorf("unexpected unmarshal error %+v", err))
-							// }
-							g.If(jen.Id("err").Op("!=").Nil()).BlockFunc(func(g *jen.Group) {
-								g.Panic(jen.Qual("github.com/juju/errors", "Errorf").Call(
-									jen.Lit("unexpected unmarshal error %+v"), jen.Id("err")))
-							})
-						}),
-				)
-			}).Line()
-	}
+							// return nil, errors.Trace(err)
+							g.Return(jen.Nil(), jen.Qual("github.com/juju/errors", "Trace").Call(jen.Id("err")))
+						}).Line()
+					// response, err := client.Do(request)
+					g.List(jen.Id("response"), jen.Id("err")).Op(":=").
+						Id("client").Dot("Do").Call(jen.Id("request"))
+					// if err != nil {
+					g.If(jen.Id("err").Op("!=").Nil()).
+						BlockFunc(func(g *jen.Group) {
+							// return nil, errors.Trace(err)
+							g.Return(jen.Nil(), jen.Qual("github.com/juju/errors", "Trace").Call(jen.Id("err")))
+						}).Line()
+					// defer response.Body.Close()
+					g.Defer().Id("response").Dot("Body").Dot("Close").Call()
+					// if response.StatusCode != http.StatusOK {
+					g.If(jen.Id("response").Dot("StatusCode")).Op("!=").Qual("net/http", "StatusOK").
+						BlockFunc(func(g *jen.Group) {
+							// return nil, errors.Errorf("call remote endpoint failed, http status %d %s", response.StatusCode, response.Status)
+							g.Return(
+								jen.Nil(),
+								jen.Qual("github.com/juju/errors", "Errorf").Call(
+									jen.Lit("call remote endpoint failed, http status %d %s"),
+									jen.Id("response").Dot("StatusCode"),
+									jen.Id("response").Dot("Status"),
+								),
+							)
+						}).Line()
+					// var resp RESP
+					g.Var().Id("resp").Id("RESP")
+					// err = json.NewDecoder(response.Body).Decode(&resp)
+					g.Id("err").Op("=").Qual("encoding/json", "NewDecoder").
+						Call(jen.Id("response").Dot("Body")).
+						Dot("Decode").Call(jen.Op("&").Id("resp"))
+					// if err != nil {
+					g.If(jen.Id("err").Op("!=").Nil()).
+						BlockFunc(func(g *jen.Group) {
+							// return nil, errors.Trace(err)
+							g.Return(jen.Nil(), jen.Qual("github.com/juju/errors", "Trace").Call(jen.Id("err")))
+						}).Line()
+					// return resp, nil
+					g.Return(jen.Id("resp"), jen.Nil())
+				}))
+		}).Line()
 
 	f.Func().Id("NewClient").
 		Params(jen.Id("host").String(), jen.Id("client").Op("*").Qual("net/http", "Client")).
 		Params(jen.Qual(pkgPath, svcName)).
 		BlockFunc(func(g *jen.Group) {
+			for i := 0; i < iface.NumMethods(); i++ {
+				method := iface.Method(i)
+				g.Id("url"+method.Name()).Op(":=").Qual("net/url", "URL").Values(
+					jen.Dict{
+						jen.Id("Scheme"): jen.Lit("https"),
+						jen.Id("Host"):   jen.Id("host"),
+						jen.Id("Path"): jen.Lit(
+							fmt.Sprintf(
+								"/api/v1/%s/%s",
+								strcase.ToKebab(svcName),
+								strcase.ToKebab(method.Name()),
+							)),
+					},
+				)
+			}
 			g.Return(jen.Id("EndpointSet").Values(jen.DictFunc(func(d jen.Dict) {
 				for i := 0; i < iface.NumMethods(); i++ {
 					method := iface.Method(i)
+					signature := method.Type().(*types.Signature)
+					params := signature.Params()
+					results := signature.Results()
 					d[jen.Id(method.Name()+"Endpoint")] = jen.
-						Id(fmt.Sprintf("Make%sRemoteEndpoint", method.Name())).
-						Call(jen.Id("host"), jen.Id("client"))
+						Id("makeRemoteEndpoint").
+						Types(generateTypeCode(params.At(1).Type()), generateTypeCode(results.At(0).Type())).
+						Call(
+							jen.Id("url"+method.Name()).Dot("String").Call(),
+							jen.Id("client"),
+						)
 				}
 			})))
 		}).Line()
@@ -167,6 +175,7 @@ func GenerateTransportLayerHTTP(f *jen.File, svc *types.Named) error {
 		Params(jen.Id("svc").Qual(pkgPath, svcName), jen.Id("m").Op("*").Qual("net/http", "ServeMux")).
 		Params(jen.Op("*").Qual("net/http", "ServeMux")).
 		BlockFunc(func(g *jen.Group) {
+
 			// TODO: 需要支持可选的 API 路径配置
 			for i := 0; i < iface.NumMethods(); i++ {
 				method := iface.Method(i)
@@ -175,7 +184,7 @@ func GenerateTransportLayerHTTP(f *jen.File, svc *types.Named) error {
 				apiServiceName := strcase.ToKebab(svcName)
 				g.Id("m").Dot("HandleFunc").Call(
 					jen.Lit(fmt.Sprintf("/api/v1/%s/%s", apiServiceName, apiEndpointName)),
-					jen.Id(fmt.Sprintf("Make%sHandlerFunc", method.Name())).Call(jen.Id("svc")),
+					jen.Id("makeHandlerFunc").Call(jen.Id("svc").Dot(method.Name())),
 				)
 			}
 			g.Return(jen.Id("m"))
